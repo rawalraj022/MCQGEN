5398082afd2d1a82f5cbd5e2e90469d18872ca26
Rajkumar Rawal <rawalraj022@gmail.com>
2024-11-04 07:23:48 +0545

    I created a logger

---
+++ test.py
@@ -0,0 +1,7 @@
+from src.mcqgenerator.logger import logging
+
+
+logging.info("hi, i am going to start my execution...")
+
+# to running this file we need to run this command in terminal
+# python test.py 
